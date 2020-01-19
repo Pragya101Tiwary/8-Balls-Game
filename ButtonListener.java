ef22fe66f99abe79d2fa6d299ee3a9df17ed8f1e
Pragya Tiwary <pragyatiwari101@gmail.com>
2020-01-19 20:14:58 +0530

    Update ButtonListener.java

--- ButtonListener.java
+++ ButtonListener.java
@@ -5,8 +5,6 @@ import java.awt.event.*;
 class ButtonListener implements ActionListener{
 	public void actionPerformed(ActionEvent evt){
 		JButton b = (JButton)evt.getSource();
-		if(b==Va.balls[0]){
-			System.out.print("YES");
-		}
+		if(b==bt[])
 	}
 }
